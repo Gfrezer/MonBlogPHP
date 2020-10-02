09d93d1e8cab73503ac0872a6d3ab6ffcbfd69d9
gael <messagerie85@gmx.fr>
2020-10-02 15:55:12 +0200

    fin du first commit

--- laravel8/app/Http/Requests/UserCreateRequest.php
+++ laravel8/app/Http/Requests/UserCreateRequest.php
@@ -2,16 +2,25 @@
 
 namespace App\Http\Requests;
 
-use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
 
-class UserCreateRequest extends Request
+class UserCreateRequest extends FormRequest
 {
-
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
     public function authorize()
     {
         return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
     public function rules()
     {
         return [
@@ -20,5 +29,4 @@ class UserCreateRequest extends Request
             'password' => 'required|confirmed|min:6',
         ];
     }
-
 }
